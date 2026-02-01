f3c86fad702625a6baf2073d0ca36900bf8cdec6
Ntwitm <18274691708@163.com>
2026-02-01 15:57:26 +0800

    mq依赖更改，三方包有问题，更改为官方推荐依赖 添加mq在application中的配置 添加mq的常量 添加放入死信队列的操作

--- hm-dianping/src/main/java/com/hmdp/service/impl/MqVoucherOrderServiceimpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/MqVoucherOrderServiceimpl.java
@@ -5,11 +5,13 @@ import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
 
+import com.hmdp.utils.RabbitMqConstants;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueInformation;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
@@ -36,22 +38,19 @@ public class MqVoucherOrderServiceimpl extends ServiceImpl<VoucherOrderMapper, V
     private RabbitTemplate rabbitTemplate;
     @Resource
     private ISeckillVoucherService seckillVoucherService;
-    private static final String queueName = "voucher_order";
-    private static final String dead_queue = "dead_voucher_order";
-
     private static final String LOCK_KET_PREFIX = "voucher:order:lock";
     private static final int MAX_RETRY = 3;
     @Resource
     private AmqpAdmin amqpAdmin;
     @PostConstruct
     public void init(){
-        createQueue(queueName);
-        createQueue(dead_queue);
+        createQueue(RabbitMqConstants.QUEUE_NAME);
+        createQueue(RabbitMqConstants.DEAD_QUEUE_NAME);
     }
 
     private void createQueue(String queueName) {
-
-        if(!queueName.equals(amqpAdmin.getQueueInfo(queueName).getName())){
+        QueueInformation exist= amqpAdmin.getQueueInfo(queueName) ;
+        if(exist==null) {
             log.info("不存在队列{},即将添加",queueName);
             Queue queue =new Queue(queueName, true, false, false);
             queue.shouldDeclare();
@@ -59,15 +58,18 @@ public class MqVoucherOrderServiceimpl extends ServiceImpl<VoucherOrderMapper, V
             log.info("队列{}创建",queueName);
         }
         else{
-            log.info("{}已存在",queueName);
+            log.info("{}已存在,详细信息{}",queueName,exist);
         }
     }
 
     @RabbitListener(queues = "voucher_order")
     public void voucherOrderHandler(Message message, Channel channel, @Header("orderId") Long orderId, @Header("voucherId")Long voucherId, @Header(AmqpHeaders.DELIVERY_TAG)Long deliveryTag,@Header("retryCounts") int retryCounts) throws IOException {
-
         if(retryCounts > MAX_RETRY){
             //TODO放入死信队列
+            channel.basicReject(deliveryTag, false);
+            deadVoucherOrderHandler(message,orderId,voucherId,deliveryTag);
+            //再次放入主队列重试
+            channel.basicNack(deliveryTag,false,true);
             return;
         }
         String key = LOCK_KET_PREFIX + orderId;
@@ -77,17 +79,22 @@ public class MqVoucherOrderServiceimpl extends ServiceImpl<VoucherOrderMapper, V
             boolean saved = save(voucherOrder);
             if (saved) {
                 try {
+                    //加个Lua检查
+                    stringRedisTemplate.delete(key);
                     channel.basicAck(deliveryTag, false);
                 } catch (IOException e) {
                     log.info("确定信息失败{}", e.getMessage());
                     retryCounts++;
-                    voucherOrderHandler(message, channel, orderId, voucherId, deliveryTag, retryCounts);
-                    throw new RuntimeException(e);
+                    //重新把消息放到前面处理
+                    channel.basicNack(deliveryTag, false,true);
+                    throw new RuntimeException(orderId+"消息确定失败,已重新投放消息",e);
                 }
             } else {
                 log.info("{}插入失败", orderId);
                 retryCounts++;
-                voucherOrderHandler(message, channel, orderId, voucherId, deliveryTag, retryCounts);
+                //是不是应该加个前提是其他人没有持有锁，Lua脚本合并redis操作？
+                stringRedisTemplate.expire(key, Duration.of(3, ChronoUnit.MINUTES));
+                channel.basicNack(deliveryTag, false,true);
             }
         }
         else{
@@ -98,7 +105,11 @@ public class MqVoucherOrderServiceimpl extends ServiceImpl<VoucherOrderMapper, V
     }
 
     @RabbitListener(queues = "dead_voucher_order")
-    public void deadVoucherOrderHandler(Message message,@Header("orderId") Long orderId, @Header("voucherId")Long voucherId){
+    public void deadVoucherOrderHandler(Message message,@Header("orderId") Long orderId,
+                                        @Header("voucherId")Long voucherId,
+                                        @Header(AmqpHeaders.DELIVERY_TAG)Long deliveryTag
+                                        ) throws IOException {
+        log.info("订单{}插入死信队列",orderId);
 
     }
 }
