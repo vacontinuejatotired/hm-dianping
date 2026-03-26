c0e15a78d0bd6f27df26452a736682cd509b5f22
Ntwitm <18274691708@163.com>
2026-03-26 14:01:52 +0800

    feat(SnowflakeIdQueue): 补充

--- hm-dianping/src/main/java/com/hmdp/entity/SnowflakeIdQueue.java
+++ hm-dianping/src/main/java/com/hmdp/entity/SnowflakeIdQueue.java
@@ -1,20 +1,86 @@
 package com.hmdp.entity;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * 批次ID队列，预先生成批次ID并存储在队列中，确保每次生成ID时都能快速获取一个唯一的批次ID
+ * 负载因子，控制批量生成ID的频率，避免过度生成导致内存占用过高
+ * 0.75表示当队列中的ID数量达到容量的75%时触发批量生成新ID的逻辑
+ * 注意不支持持久化，重启后会丢失数据
+ * ID刷新逻辑需在调用方实现，调用方根据返回值决定是否触发异步刷新逻辑，避免过度耦合
+ * @author Ntwitm
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class SnowflakeIdQueue {
     /**
      * 批次ID队列，预先生成批次ID并存储在队列中，确保每次生成ID时都能快速获取一个唯一的批次ID
      */
-    private static final LinkedBlockingQueue<Long> BATCH_ID_QUEUE=new LinkedBlockingQueue<Long>();
+    private static final LinkedBlockingQueue<Long> BATCH_ID_QUEUE = new LinkedBlockingQueue<Long>();
 
     /**
      * 负载因子，控制批量生成ID的频率，避免过度生成导致内存占用过高
-     * 0.75表示当队列中的ID数量达到容量的75%时触发批量生成新ID的逻辑
-     * 没有测试过。仅仅是想到HashMap中扩容时使用的负载因子，感觉可以借鉴一下，暂时设置为0.75，后续可以根据实际情况调整
+      * 0.75表示当队列中的ID数量达到容量的75%时停止批量生成新ID的逻辑
+      * 0.25表示当队列中的ID数量达到容量的25%时触发批量生成新ID的逻辑，提供更早的预警
      */
-    private static final Double DEFAULT_LOAD_FACTOR = 0.75;
+    private static final Double  DEFAULT_MIN_LOAD_FACTOR = 0.25 ;
+    private static final Double  DEFAULT_MAX_LOAD_FACTOR = 0.75 ;
 
-    private static final Long INIT_CAPICITY = 10000L;
+    private  int initCapacity = 10000;
 
+    /**
+     * 共享锁，控制批量生成ID的线程安全，避免多个线程同时触发批量生成导致过度生成ID
+      * 使用AtomicBoolean作为锁标志，确保只有一个线程能够执行批量生成ID的逻辑
+      * 当一个线程正在执行批量生成ID时，其他线程无法进入该逻辑，直接返回
+     */
+    private final   AtomicBoolean isRefreshing = new AtomicBoolean(false);
+
+
+    /**
+     * 这里让其他地方做异步刷新，不在本类里实现刷新功能了，避免过度耦合
+     * 返回-1表示需要刷新，调用方根据返回值决定是否触发异步刷新逻辑
+     * @return
+     * @throws InterruptedException
+     */
+    public Long take() throws InterruptedException {
+        if (checkRefresh()) {
+            return -1L;
+        }
+        Long id = BATCH_ID_QUEUE.take();
+        return id;
+    }
+
+    public void put(Long id) throws InterruptedException {
+        BATCH_ID_QUEUE.put(id);
+    }
+
+    public int size() {
+        return BATCH_ID_QUEUE.size();
+    }
+    /**
+     * 根据实际容量决定是否需要批量生成新ID
+     * 加锁仅允许一个线程执行批量生成ID的逻辑，避免多个线程同时触发批量生成导致过度生成ID
+     */
+    public  boolean checkRefresh() {
+        if (size() < initCapacity * DEFAULT_MIN_LOAD_FACTOR) {
+            if (isRefreshing.compareAndSet(false, true)) {
+                // 调用方需手动设置isRefreshing为false，刷新成功手动设置为false，刷新失败也要设置为false，避免死锁
+                return true;
+            }
+        }
+        if (size() > initCapacity * DEFAULT_MAX_LOAD_FACTOR) {
+            isRefreshing.set(false);
+        }
+        return false;
+    }
+
+    public void finishRefresh() {
+        isRefreshing.set(false);
+    }
 }
